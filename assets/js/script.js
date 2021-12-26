dc2ab107835de6122db32643984c0bfd5c968352
nickus1028 <bentivolio@gmail.com>
2021-12-26 17:28:45 -0500

    added questions variables. Initial CSS styling and HTML elements

---
+++ assets/js/script.js
@@ -0,0 +1,87 @@
+var TimeLeft = 6000;
+var currentQuestion = 0;
+
+//Quiz Questions stored in an array
+
+var Questions = [     
+    {
+    
+        question: "Arrays in JavaScript can be used to store _______.",
+        
+        answers: {
+            1:"numbers and strings",
+    
+            2:"other arrays",
+    
+            3:"booleans",
+    
+            4:"all of the above"
+        },
+    
+        correctAnswer:'4'
+    },
+    {
+        question: "A very useful tool used during development and debuggging for printing content to the debugger is:",
+        
+        answers: {
+            1:"JavaScript",
+    
+            2:"terminal/bash",
+    
+            3:"console.log",
+    
+            4:"for loops"
+        },
+    
+        correctAnswer:'3'
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        
+        answers: {
+            1:"quotes",
+    
+            2:"curly brackets",
+    
+            3:"commas",
+    
+            4:"parenthesis"
+        },
+    
+        correctAnswer:'1'
+    },
+    {
+        question: "Commonly used data types DO NOT include:",
+        
+        answers: {
+            1:"strings",
+    
+            2:"booleans",
+     
+            3:"alerts",
+    
+            4:"numbers"
+        },
+    
+        correctAnswer:'3'
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript",
+        
+        answers: {
+            1:"<javascript>",
+    
+            2:"<script>",
+    
+            3:"<js>>",
+    
+            4:"<scripting>"
+        },
+    
+        correctAnswer:'2'
+    },
+]
+
+
+
+
